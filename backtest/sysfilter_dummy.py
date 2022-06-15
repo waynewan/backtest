f0b45589ba25bc325ccf472fa106934ce9deb7a3
wayne.wan.peek <12496043+waynewan@users.noreply.github.com>
2022-06-15 14:50:47 -0400

    dummy filter added

---
+++ backtest/sysfilter_dummy.py
@@ -0,0 +1,23 @@
+from backtest import norgate_helper as ngu
+from backtest.abc import sysfilter_abc
+import pandas as pd
+# --
+# --
+# --
+class sysfilter_dummy(sysfilter_abc.sysfilter_abc):
+	def __init__(self,*,opt):
+		super().__init__()
+		self.__opt = opt
+
+	def allow_entry(self,dt):
+		return True
+
+	def allow_exit(self,dt):
+		return True
+
+	# --
+	# --
+	# --
+	def _read_opt(self): return self.__opt
+	opt = property(_read_opt,None,None)
+
