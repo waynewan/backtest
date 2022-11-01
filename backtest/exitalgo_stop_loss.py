d061cdc20f646fb39e980d29e917102f3caef78a
wayne.wan.peek <12496043+waynewan@users.noreply.github.com>
2022-11-01 07:41:13 -0400

    add option to delay stoploss, and static stoploss

--- backtest/exitalgo_stop_loss.py
+++ backtest/exitalgo_stop_loss.py
@@ -1,6 +1,7 @@
-from jackutil.microfunc import callable_fq_name
+from jackutil.microfunc import callable_fq_name,dt64_to_dt
 from backtest.abc.exitalgo_abc import exitalgo_abc
 from backtest import norgate_helper
+from datetime import timedelta
 import pandas as pd
 import numpy as np
 
@@ -33,12 +34,26 @@ class exitalgo_stop_loss(exitalgo_abc):
 			return "stop_loss_reach"
 		return None
 
+	def get_stoploss_anchor_price(self,pos,bar):
+		if(self.__opt.get("trailing_stop_loss",True)):
+			return bar['Close']
+		else:
+			return pos.entry_price
+
+	def is_stoploss_effective(self,dt,pos):
+		effective_after_days = self.__opt.get("effective_after",0)
+		effective_after_date = pos.entry_exec_date + np.timedelta64(effective_after_days,'D')
+		return dt >= effective_after_date
+
 	def calc_all_exit_conditions(self,dt,pos,bar,bars,universe):
+		new_val = 0
+		if(self.is_stoploss_effective(dt,pos)):
+			new_val = ( 1 - self.__opt["stop_loss_pct"] / 100.00 ) * self.get_stoploss_anchor_price(pos,bar)
 		return {
 			"stop_loss" : {
 				'upd_date' : dt,
 				'upd_msg' : None,
-				'new_val' : bar['Close'] * ( 1 - self.__opt["stop_loss_pct"] / 100.00 ),
+				'new_val' : new_val,
 			},
 		}
 
