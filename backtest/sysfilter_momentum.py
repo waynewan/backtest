7f69cfb432c7895caf10a8cc92dccd19e2900b83
wayne.wan.peek <12496043+waynewan@users.noreply.github.com>
2023-03-11 21:08:03 -0500

    add ptype:PRICE as option, compute 1yr change>=0

--- backtest/sysfilter_momentum.py
+++ backtest/sysfilter_momentum.py
@@ -15,17 +15,25 @@ class sysfilter_momentum(sysfilter_abc.sysfilter_abc):
 		self.__load(**self.__opt)
 
 	def __load(self,*,symbol,benchmark,period,window,offset,ptype,smooth):
-		bm = self.__dsrc.load_history_for_symbol(benchmark)
 		hist0 = self.__dsrc.load_history_for_symbol(symbol)
 		hist0['r0'] = 100 * ( hist0['Close'] / hist0['Close'].shift(period) - 1)
-		self._d0 = hist0
+		self._d0 = hist0.copy()
+		if(ptype=="PRICE"):
+			self.__smooth_function(smooth,window)
+			return
 		# --
+		bm = self.__dsrc.load_history_for_symbol(benchmark)
 		if(ptype=="YIELD"):
 			self._d0['r0'] = hist0['r0'] - bm['Close'] - offset
 		elif(ptype=="CASH"):
 			bm['r0'] = 100 * ( bm['Close'] / bm['Close'].shift(period) - 1)
 			self._d0['r0'] = hist0['r0'] - bm['r0'] - offset
-		# --
+		else:
+			raise ValueError(f"unknown ptype:{ptype}")
+		self.__smooth_function(smooth,window)
+		return
+
+	def __smooth_function(self,smooth,window):
 		if(smooth=='min'):
 			self._d0['r0'] = self._d0['r0'].rolling(window).min()
 		elif(smooth=='max'):
@@ -35,7 +43,7 @@ class sysfilter_momentum(sysfilter_abc.sysfilter_abc):
 
 	def allow_entry(self,dt):
 		if(dt in self._d0.index):
-			return self._d0.loc[dt,'r0'] > 0.0
+			return self._d0.loc[dt,'r0'] >= 0.0
 		return False
 
 	def allow_exit(self,dt):
